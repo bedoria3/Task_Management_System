49c277dd50c661644c89f726e7eba906e192d16d
xycys <113002797+xycystheia@users.noreply.github.com>
2022-12-11 08:02:44 +0800

    Add files via upload

---
+++ Task_Management_System/src/task_management_system/Main.java
@@ -0,0 +1,17 @@
+package task_management_system;
+
+
+import task_management_system.task_management_system;
+
+
+
+     
+    
+public class Main {
+     public static void main(String args[]) {
+         task_management_system lp = new task_management_system();
+         lp.setVisible(true);
+         lp.pack();
+         
+    }
+}
